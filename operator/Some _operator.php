5ace7d902cd517cc76f4af35f5bbd700bd2cd600
shakawat777 <hossainshakawat25@gmail.com>
2024-06-01 13:49:46 +0600

    all operators are complete

--- operator/Some _operator.php
+++ operator/Some _operator.php
@@ -1,5 +1,44 @@
+
+
 <?php
+
+//Addition
+/*
 $x=5;
 $y=8;
 $sum=$x+$y;
 echo"sum = $sum";
+*/
+
+//Subtraction
+/*
+$x=85;
+$y=50;
+$subtract=$x-$y;
+echo"subtract = $subtract";
+*/
+
+//multiply
+/*
+$x=5;
+$y=8;
+$multiply=$x*$y;
+echo"multiply = $multiply";
+*/
+
+//divide
+/*
+$x=80;
+$y=5;
+$divide=$x/$y;
+echo"divide = $divide";
+*/
+
+//modulus
+
+$x=80;
+$y=9;
+$modulus=$x%$y;
+echo"modulus = $modulus";
+?>
+
