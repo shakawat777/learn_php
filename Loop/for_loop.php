b9e2698089e954c4c35c22bb0c24989f14a6251d
shakawat777 <hossainshakawat25@gmail.com>
2024-06-02 14:17:53 +0600

    nested loop start

--- Loop/for_loop.php
+++ Loop/for_loop.php
@@ -8,14 +8,51 @@ for($i = 0; $i < $count; $i = $i+2) {
     }
 }
 */
+// Sumation of the even and odd numbers using for loop.
+/*
 $n=10;
 $sum = 0;
- for($i = 0; $i < $n; $i++){
-
-    $sum = $sum + $i;
-    echo "$sum ";
+ for($i = 0; $i < $n; $i++){ 
+    if($i % 2==0){
+        $sum = $sum + $i;
+        echo "$sum ";
+    } 
  }
  
+*/
+// Using nested for loops.
+// $i;
+// $j;
+
+/*
+for($i = 1; $i <= 5; $i++){
+    for($j = 1; $j <= 5; $j++){
+        echo $i*$j. " ";
+    }
+    echo "\n";
+}
+*/
+
+// Find power using loops.
+// echo pow(2,3);
+// echo"\n";
+
+
+/*
+$m=1;
+$b=2;
+for ($i=1;$i<=3;$i++) {
+    $m=$m*$b;
+}
+echo $m;
+*/
+
+for ($i=1;$i<=3;$i++) {
+
+    echo $i;
+    echo "\n";
+}
+ echo "$i";
 
 
 ?>
