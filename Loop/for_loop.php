1f2b6cf36695470cf06748d7ef0a48ab721ca780
shakawat777 <hossainshakawat25@gmail.com>
2024-06-02 12:49:16 +0600

    loop part start

---
+++ Loop/for_loop.php
@@ -0,0 +1,21 @@
+<?php
+/*
+$count = 20;
+for($i = 0; $i < $count; $i = $i+2) {
+
+    if($i%2 == 0) {
+        echo "$i ";
+    }
+}
+*/
+$n=10;
+$sum = 0;
+ for($i = 0; $i < $n; $i++){
+
+    $sum = $sum + $i;
+    echo "$sum ";
+ }
+ 
+
+
+?>
