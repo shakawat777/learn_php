6f5aa2a86c686fbd6331abe61041aefa40701210
shakawat777 <hossainshakawat25@gmail.com>
2024-06-04 17:47:36 +0600

    array section complete

--- Array/exploed.php
+++ Array/exploed.php
@@ -1,4 +1,28 @@
 <?php
+/*
 $fruits = "Apple, Banana,orange";
-$c = explode(',', $fruits);
+$c = explode(',', $fruits);//$c = explode(" ", $fruits);
 print_r($c);
+*/
+
+/*
+$fruits = "Apple, Banana,orange,mango,cheery";
+$c = preg_split("/[\s,]+/", $fruits);
+print_r($c);
+
+echo"\n";
+*/
+
+$fruits = "Apple,Banana,orange,mango,cheery";
+$c = preg_split("/\s*/", $fruits);
+
+print_r($c);
+/*
+echo"\n";
+$arr = array('Hello','World!','Beautiful','Day!');
+echo implode(" ",$arr);
+*/
+echo"\n";
+
+$arr = array('Hello','World!','Beautiful','Day!');
+echo join(" ",$arr);
