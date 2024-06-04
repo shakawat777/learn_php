c18dd3a930a39b962c503b5410fe1b1ce1ef3442
shakawat777 <hossainshakawat25@gmail.com>
2024-06-04 22:34:37 +0600

    basic function

---
+++ Function/bsaic.php
@@ -0,0 +1,28 @@
+<?php
+/*
+function evenOrodd($n) {
+    if($n%2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+$x=12;
+if(evenOrodd($x)){
+    echo "The number {$x} is even";
+}
+else{
+    echo "The number {$x} is odd";
+}
+*/
+
+function sum(){
+    $a = 35;
+    $b = 45;
+    $c=5;
+    $sum = $a+$b+$c;
+    echo "$sum";
+
+}
+sum();
