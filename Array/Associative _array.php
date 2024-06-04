628d6389944eddabec57b3006d928e0c45be4a48
shakawat777 <hossainshakawat25@gmail.com>
2024-06-04 12:22:27 +0600

    second part of associative array

--- Array/Associative _array.php
+++ Array/Associative _array.php
@@ -1,5 +1,31 @@
 <?php
+/*
+//Syntax foreach loop:
 $myArray = array("name" => "volvo", "age" => "24", "message" => "medin Germany" );
-foreach ($myArray as $value){
-    echo "$value\n";
+
+
+foreach ($myArray as $key => $value){
+    echo "$key => $value\n";
 }
+*/
+
+function myCar(){
+    echo "volvo";
+   
+ }
+function myMessage(){
+     echo "medin Germany";
+}
+
+function myAge(){
+    echo "24";
+}
+$myArray = array("car" => "myCar", "age" => "myAge", "message" => "myMessage" );
+
+ $myArray["car"]();
+ echo "\n";
+ $myArray["age"]();
+ echo "\n";
+ $myArray["message"]();
+
+ 
