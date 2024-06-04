bbd5d931cc6e83fc12869578a9ca03ef31f43295
shakawat777 <hossainshakawat25@gmail.com>
2024-06-04 13:17:44 +0600

    explord function done

--- Array/Associative _array.php
+++ Array/Associative _array.php
@@ -9,6 +9,9 @@ foreach ($myArray as $key => $value){
 }
 */
 
+
+
+/*
 function myCar(){
     echo "volvo";
    
@@ -27,5 +30,40 @@ $myArray = array("car" => "myCar", "age" => "myAge", "message" => "myMessage" );
  $myArray["age"]();
  echo "\n";
  $myArray["message"]();
+*/
+
+$foods=[
+    "vegetables" => "
+    Tomato,
+    Potato,
+    Cucumber,
+     Pumpkin,
+     Pea",
+    "fruits" => "
+    Apple,
+    Orange,
+    Banana,
+    Grapes,
+    Mango",
+    "Drinks" => "
+    Lemu, 
+    Rc-cola,
+    mojo,
+    frutika,
+    Lemon,
+    "
+];
+// This line for concatenation.
+$foods["Drinks"] .= "cleamon";
+
+foreach ($foods as $key => $food) {
+     echo "$key" ."=>" . "$food";
+     echo "\n";
+}
+// For searching key.
+/*
+$key = array_keys($foods);
+print_r($key);
+*/
 
  
---
+++ Array/exploed.php
@@ -0,0 +1,4 @@
+<?php
+$fruits = "Apple, Banana,orange";
+$c = explode(',', $fruits);
+print_r($c);
