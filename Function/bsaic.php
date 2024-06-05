ece09f452eddd9d342ff143f857026fda1519350
shakawat777 <hossainshakawat25@gmail.com>
2024-06-05 13:22:52 +0600

    start recursion

--- Function/bsaic.php
+++ Function/bsaic.php
@@ -17,12 +17,41 @@ else{
 }
 */
 
-function sum(){
-    $a = 35;
-    $b = 45;
-    $c=5;
+/*
+function sum(float $a, int $b, int $c){
+    // $a = 35;
+    // $b = 45;
+    // $c=5;
     $sum = $a+$b+$c;
     echo "$sum";
 
 }
-sum();
+sum(3.5,5,6);
+*/
+// Most important code for function.
+/*
+function sum(...$numbers) {
+    $sum = 0;
+    for ($i=0;$i<count($numbers);$i++) {
+        $sum+= $numbers[$i];
+    }
+    return $sum;
+    
+
+}
+echo  sum(1,2,3,4,5);
+*/
+/* Write a function for recursive function.*/ 
+
+function printNumbers($numbers){
+    if($numbers > 10){
+        return;
+
+    }
+    echo $numbers . " ";
+    printNumbers($numbers+1);
+}
+printNumbers(0);
+
+ 
+
