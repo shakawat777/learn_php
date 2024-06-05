cab24b4c7f2535007e3f25d226e77a624c5b53cc
shakawat777 <hossainshakawat25@gmail.com>
2024-06-05 20:57:39 +0600

    time function start

--- Function/recursion.php
+++ Function/recursion.php
@@ -12,6 +12,8 @@ function printNumbers($numbers){
 }
 printNumbers(0);
 */
+
+/*
 // Write a program SWAP two numbers.
 $a = 10;
 $b = 15;
@@ -22,5 +24,38 @@ $b = 15;
     $a = $b;
     $b = $temp;
     echo "a = " . $a. "\n". "b = ". $b. "\n";
+*/
+    /* Write a program swap two numbers using recursion*/
+/*
+    function swap($a, $b) {
+        $temp = $a;
+        $a = $b;
+        $b = $temp;
+         echo "a = ". $a. "\n". "b = ". $b. "\n";
+        echo "\n";
 
+    }
+    
+    $a = 10;
+    $b = 15;
+    swap($a, $b);
+    // echo "a = ". $a. "\n". "b = ". $b. "\n";
+    */
+//Write a program fibonacci series using recursive functions.
+/*
+function fibonacci($old, $new, $start, $end){
+    if($start>$end){
+        return;
+    }
+    $start++;
+    echo $old . " ";
+    $temp = $old + $new;
+    $old = $new; 
+    $new = $temp;
+    fibonacci($old, $new, $start, $end);
+} 
+fibonacci(0, 1, 1, 10)
+
+*/
+echo date("F l  d-y-m  h:i:s");
 ?>
