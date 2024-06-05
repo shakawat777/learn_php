4d1e6fe960b9bebaa23e30181950d2db26b02d78
shakawat777 <hossainshakawat25@gmail.com>
2024-06-05 13:32:09 +0600

    Swap two numbers

---
+++ Function/recursion.php
@@ -0,0 +1,26 @@
+<?php
+
+/* Write a function for recursive function.*/ 
+/*
+function printNumbers($numbers){
+    if($numbers > 10){
+        return;
+
+    }
+    echo $numbers . " ";
+    printNumbers($numbers+1);
+}
+printNumbers(0);
+*/
+// Write a program SWAP two numbers.
+$a = 10;
+$b = 15;
+ echo "a = " . $a . "\n" ."b = " . $b ."\n";
+ echo "\n";
+ // logic for swapping:
+    $temp =  $a;
+    $a = $b;
+    $b = $temp;
+    echo "a = " . $a. "\n". "b = ". $b. "\n";
+
+?>
