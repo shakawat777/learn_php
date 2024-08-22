e1c044715761a8ddfae281dd7df5255059c1ecee
shakawat777 <hossainshakawat25@gmail.com>
2024-08-22 12:48:47 +0600

    file name extention

--- class-12/fileprocess.php
+++ class-12/fileprocess.php
@@ -1,8 +1,44 @@
 <?php
-$path_parts = pathinfo('/www/htdocs/inc/lib.inc.php');
+/*
+$path_parts = pathinfo('/www/htdocs/inc/photos.png');
 
 echo $path_parts['dirname'], "\n";
 echo $path_parts['basename'], "\n";
 echo $path_parts['extension'], "\n";
 echo $path_parts['filename'], "\n";
+*/
+
+/*
+// Get all path infomations.
+$path = "path/to/images/photo.jpg";
+$info = pathinfo($path);
+print_r($info);
+Output:
+Array
+(
+    [dirname] => path/to/images
+    [basename] => photo.jpg
+    [extension] => jpg
+    [filename] => photo
+)
+    */
+
+
+    /*
+    //Get the specific part of the path:
+        $path = "path/to/images/photo.jpg";
+        $dirname = pathinfo($path, PATHINFO_DIRNAME);
+        echo $dirname; //Output: path/to/images
+        echo "\n";
+        $basename = pathinfo($path, PATHINFO_BASENAME);
+        echo $basename; //Output: photo.jpg
+        echo "\n";
+        $extension = pathinfo($path, PATHINFO_EXTENSION);
+        echo $extension; //Output: jpg
+        echo "\n";
+        $filename = pathinfo($path, PATHINFO_FILENAME);
+        echo $filename; //Output: photo
+
+        */
+        
 ?>
